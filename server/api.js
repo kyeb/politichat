f2158a63a8ff32e85ada9804b48322844502f9b7
Sabrina Chern <sabrinachern@sabrinas-mbp.attlocal.net>
2020-04-09 22:36:12 -0700

    added username for each room

--- server/api.js
+++ server/api.js
@@ -25,39 +25,38 @@ router.post("/newroom", (req, res) => {
   // generate random room ID number
   const roomID = Math.random().toString(36);
 
-  // make a room object with keys id and roomName
-  const room_temp = {id: roomID, roomName: req.body.roomName};
+  // make a room object with keys id, roomName, user
+  const room_temp = {id: roomID, roomName: req.body.roomName, user: req.user.username};
 
   // add room object to array of rooms
   rooms.push(room_temp);
-  console.log(rooms)
+  
   // then, send back the entire room object
   res.send(room_temp)
 });
 
-router.post("/deleteroom", (req, res) => {
-  // delete room by its ID from the array rooms
+router.get("/deleteroom", (req, res) => {
+  // delete a room by its ID from the array rooms
   rooms = rooms.filter(function (e) {
-    return e.id = req.query.id
+    return e.id !== req.query.id; 
   })
 }); 
 
 router.get("/rooms", (req, res) => {
-  // returns all available rooms with names and IDs in an array
+  // returns all available rooms with id and roomName in an array
   res.send(rooms)
 });
 
 router.get("/room", (req,res) => {
   // returns just the room with a specific requested ID
-  req.query.id
+  // each room should have the id and roomName so far
+  res.send(rooms.find(e => e.id === req.query.id)); 
 });
 
 router.get("/example", (req, res, next) => {
   logger.info("Log Hello World");
   res.send({ hello: "world" });
 });
-
-
 
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
