6e23176e8fa85ff1256c64f7acf48fad7468f4e3
Sabrina Chern <sabrinachern@sabrinas-mbp.attlocal.net>
2020-04-09 20:02:54 -0700

    w/ array of rooms

--- server/api.js
+++ server/api.js
@@ -15,19 +15,49 @@ const logger = require("pino")(); // use pino logger
 const User = require("./models/user");
 const Room = require("./models/room");
 
+// array to store rooms
+let rooms = []; 
+
 // api endpoints: all these paths will be prefixed with "/api/"
-const router = express.Router();
+const router = express.Router()
+
+router.post("/newroom", (req, res) => {
+  // generate random room ID number
+  const roomID = Math.random().toString(36);
+
+  // make a room object with keys id and roomName
+  const room_temp = {id: roomID, roomName: req.body.roomName};
+
+  // add room object to array of rooms
+  rooms.push(room_temp);
+  console.log(rooms)
+  // then, send back the entire room object
+  res.send(room_temp)
+});
+
+router.post("/deleteroom", (req, res) => {
+  // delete room by its ID from the array rooms
+  rooms = rooms.filter(function (e) {
+    return e.id = req.query.id
+  })
+}); 
+
+router.get("/rooms", (req, res) => {
+  // returns all available rooms with names and IDs in an array
+  res.send(rooms)
+});
+
+router.get("/room", (req,res) => {
+  // returns just the room with a specific requested ID
+  req.query.id
+});
 
 router.get("/example", (req, res, next) => {
   logger.info("Log Hello World");
   res.send({ hello: "world" });
 });
 
-router.get("/rooms", (req, res) => {
-  Room.find({}).then((rooms) => {
-    res.send(rooms);
-  });
-});
+
 
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
