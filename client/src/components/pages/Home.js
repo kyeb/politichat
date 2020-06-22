8675596e9fccf6aaf34cc0fe7d411f6fed9b5965
Kye Burchard <kyeb@mit.edu>
2020-06-21 23:44:23 -0600

    Separate logged in / out homes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,26 +1,14 @@
 import React, { Component } from "react";
 import AuthController from "../modules/AuthController";
 import RoomList from "../modules/RoomList";
-import { Form, Input, Label, Message, Divider, Loader } from "semantic-ui-react";
-import { get, post } from "../../utilities";
+import LoggedInHome from "./LoggedInHome";
+import { Form, Input, Label, Loader } from "semantic-ui-react";
 import { navigate } from "@reach/router";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      joinRoomError: "",
-      joinRoomId: "",
-      newDisplayName: "",
-      roomIds: -1, // placeholder indicating hasn't loaded
-    };
-  }
-
-  componentDidMount() {
-    get("/api/room/list").then((rooms) => {
-      let ids = rooms.map((room) => room.id);
-      this.setState({ roomIds: ids });
-    });
+    this.state = {};
   }
 
   handleCreateRoom = () => {
@@ -41,19 +29,6 @@ class Home extends Component {
     } else {
       navigate(`/room/${roomId}`);
     }
-  };
-
-  handleUpdateDisplayName = () => {
-    post("/api/user/displayname", { displayName: this.state.newDisplayName })
-      .then((res) => {
-        if (res.success) {
-          this.props.user.displayName = this.state.newDisplayName;
-          this.setState({ newDisplayName: "" });
-        }
-      })
-      .catch((err) => {
-        alert("Error when updating display name. See console for more details.");
-      });
   };
 
   render() {
@@ -109,58 +84,7 @@ class Home extends Component {
       </>
     );
 
-    let newRoomForm;
-    if (this.props.user && this.props.user.canCreateRooms) {
-      newRoomForm = (
-        <div className="newroom-container">
-          <h2>Create a new room</h2>
-          <Form>
-            <Form.Button primary className="createroom-button" onClick={this.handleCreateRoom}>
-              Create room
-            </Form.Button>
-          </Form>
-        </div>
-      );
-    } else {
-      newRoomForm = (
-        <div>
-          <h2>Create a new room</h2>
-          <Message negative>
-            You do not have permissions to create new rooms yet. <br /> Please email us at{" "}
-            <a href="mailto:politichat@mit.edu?subject=Politichat Beta access request">
-              politichat@mit.edu
-            </a>{" "}
-            for information on how to gain access to our beta.
-          </Message>
-        </div>
-      );
-    }
-
-    const updateDisplayName = (
-      <Form>
-        <h3>Update your display name</h3>
-        <p>This is the name users will see when joining your rooms.</p>
-        <Input
-          placeholder={this.props.user?.displayName}
-          action={{ content: "Update", onClick: this.handleUpdateDisplayName, primary: true }}
-          onChange={(event) => this.setState({ newDisplayName: event.target.value })}
-          value={this.state.newDisplayName}
-        />
-      </Form>
-    );
-
-    const loggedInLanding = (
-      <>
-        <div className="twocolumn">
-          <div className="Home-joinroom">{joinRoomForm}</div>
-          <div className="Home-newroom">{newRoomForm}</div>
-        </div>
-        <Divider />
-        <RoomList user={this.props.user?.username} />
-        <Divider />
-        {updateDisplayName}
-      </>
-    );
+    const loggedInLanding = <LoggedInHome user={this.props.user} />;
 
     // Render the homePage if this.props.user exists (user is logged in),
     //   else render login page
